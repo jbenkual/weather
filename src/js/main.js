0ed3de1cb56d0b21126422e16e6ba8093b653c58
jbenkual <joe@benkual.com>
2015-09-04 11:47:30 -0700

    Fixed style issues using jshint and jsformat

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
+/*jshint -W030 */ // allow expressions such as 'use strict'
 require("../index.css");
-require("../../node_modules/bootstrap-webpack/index.js");
+require("bootstrap-webpack");
 var $ = require("jquery");
 
 'use strict';
@@ -14,7 +15,7 @@ var loc = "autoip";
 var city = "";
 var state = "";
 var zip = "";
-var degreeCountries = ["USA", "Belize", "Cayman Islands", "Palau"]
+var degreeCountries = ["USA", "Belize", "Cayman Islands", "Palau"];
 
 var metric = true;
 
@@ -24,14 +25,14 @@ function init() {
 
 function goClicked(e) {
 	var newLoc = $("#uname");
-	if(newLoc.val().length > 0) {
+	if (newLoc.val().length > 0) {
 		loc = newLoc.val();
 	}
 	$(".container tbody").empty();
 	requestData(url1 + loc + ".json", processData);
 }
 
-function requestData (url, callback) {
+function requestData(url, callback) {
 	var promise = $.ajax(url, {
 		method: 'get',
 		dataType: 'jsonp'
@@ -41,22 +42,22 @@ function requestData (url, callback) {
 	});
 }
 
-function processData (data) {
+function processData(data) {
 	console.log(data);
 	city = data.location.city;
 	state = data.location.state;
-	if(state == "") {
+	if (state === "") {
 		state = data.location.country_name;
 	}
-	zip = data.location.zip
+	zip = data.location.zip;
 	metric = true;
-	if(degreeCountries.indexOf(data.location.country_name) !== -1) {
+	if (degreeCountries.indexOf(data.location.country_name) !== -1) {
 		metric = false;
 	}
 	$("#loc").text(city + ", " + state + "  " + zip);
-	requestData("http://api.wunderground.com/api/77b4e7571579589f/astronomy/q/"+state+"/"+city+".json", processTime);
-	requestData("http://api.wunderground.com/api/77b4e7571579589f/webcams/q/"+state+"/"+city+".json", processCams);
-	requestData("http://api.wunderground.com/api/77b4e7571579589f/forecast10day/q/"+state+"/"+city+".json", process10day);
+	requestData("http://api.wunderground.com/api/77b4e7571579589f/astronomy/q/" + state + "/" + city + ".json", processTime);
+	requestData("http://api.wunderground.com/api/77b4e7571579589f/webcams/q/" + state + "/" + city + ".json", processCams);
+	requestData("http://api.wunderground.com/api/77b4e7571579589f/forecast10day/q/" + state + "/" + city + ".json", process10day);
 }
 
 function process10day(data) {
@@ -68,62 +69,59 @@ function process10day(data) {
 	var trD2 = $("<tr>");
 	var trN2 = $("<tr>");
 
-	for(var i = 0; i < arr.length && i < (arr2.length * 2); i++) {
+	for (var i = 0; i < arr.length && i < (arr2.length * 2); i++) {
 		var td = $("<td>");
 		td.addClass("gridBox");
 
 		var span = $("<span>");
-		
+
 		var hl = $("<div>");
-		if(i % 2 != 0){
-			hl.html("Low: " + arr2[Math.floor(i/2)].low.fahrenheit);
+		if (i % 2 !== 0) {
+			hl.html("Low: " + arr2[Math.floor(i / 2)].low.fahrenheit);
 			hl.addClass("low");
-		}
-		else {
-			hl.html("High: " + arr2[Math.floor(i/2)].high.fahrenheit);
+		} else {
+			hl.html("High: " + arr2[Math.floor(i / 2)].high.fahrenheit);
 			hl.addClass("high");
 		}
 
 		var hlM = $("<td>");
-		if(i % 2 != 0){
-			hlM.html(" Low: " + arr2[Math.floor(i/2)].low.celsius);
-		}
-		else {
-			hlM.html("High: " + arr2[Math.floor(i/2)].high.celsius);
+		if (i % 2 !== 0) {
+			hlM.html(" Low: " + arr2[Math.floor(i / 2)].low.celsius);
+		} else {
+			hlM.html("High: " + arr2[Math.floor(i / 2)].high.celsius);
 		}
 		hlM.addClass("low");
 
 		var img = $("<img>");
-		img.attr("src", arr[i].icon_url)
+		img.attr("src", arr[i].icon_url);
 
 		span.append(img, hl, hlM);
-		
-		if(i%2 == 0) {
-			var h3 = $("<h4>");
-			h3.addClass("dayTitle");
-			h3.text(arr[i].title.substr(0,3))
-		}
-		if(metric) {
+
+
+		var h3 = $("<h4>");
+		h3.addClass("dayTitle");
+		h3.text(arr[i].title.substr(0, 3));
+
+		if (metric) {
 			hl.hide();
-		}
-		else {
+		} else {
 			hlM.hide();
 		}
 
-		td.append(span, img, h3);
-		if (i % 2 == 0) {
-			if(i > 9) {
-				trD2.append(td)
-			}
-			else {
+		td.append(span, img);
+		if (i % 2 === 0) {
+			td.append(h3);
+		}
+		if (i % 2 === 0) {
+			if (i > 9) {
+				trD2.append(td);
+			} else {
 				trD.append(td);
 			}
-		}
-		else {
-			if(i > 9) {
-				trN2.append(td)
-			}
-			else {
+		} else {
+			if (i > 9) {
+				trN2.append(td);
+			} else {
 				trN.append(td);
 			}
 		}
@@ -133,15 +131,15 @@ function process10day(data) {
 }
 
 function processCams(data) {
-	for(var i = 0; i < 5 && i < data.webcams.length; i++){
+	for (var i = 0; i < 5 && i < data.webcams.length; i++) {
 		$("#webcam" + i).attr('src', data.webcams[i].CURRENTIMAGEURL);
 	}
 }
 
 function processTime(data) {
 	var phase = data.moon_phase;
-	$("#time").html("Current Time: "+ phase.current_time.hour +":" + phase.current_time.minute);
-	$("#sunrise").html("Sunrise: "+ phase.sunrise.hour +":" + phase.sunrise.minute);
-	$("#sunset").html("Sunset: "+ phase.sunset.hour +":" + phase.sunset.minute);	
+	$("#time").html("Current Time: " + phase.current_time.hour + ":" + phase.current_time.minute);
+	$("#sunrise").html("Sunrise: " + phase.sunrise.hour + ":" + phase.sunrise.minute);
+	$("#sunset").html("Sunset: " + phase.sunset.hour + ":" + phase.sunset.minute);
 }
 requestData(url1 + loc + ".json", processData);
--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
   module: {
     loaders: [
         //{ test: /\.css$/, loader: "style!css" }
-        {test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
-        {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+        /*{test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+        {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},*/
         {test: /\.css$/, loader: 'style-loader!css-loader'},
         {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
         {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
